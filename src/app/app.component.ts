25b5ac939bf4a46a64da091c903bf850434cbcdb
kreechan123 <librandochristian92@gmail.com>
2020-03-13 22:56:19 +0800

    Assim initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'joke',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'assim';
+  jokes: Object[];
+
+  constructor() {
+    this.jokes = [
+      {
+      setup: "What did the cheese say when it looked in the mirror?",
+      punchline: "Hello-Me (Halloumi)"
+      },
+      {
+      setup: "What kind of cheese do you use to disguise a small horse?",
+      punchline: "Mask-a-pony (Mascarpone)"
+      },
+      {
+      setup: "A kid threw a lump of cheddar at me",
+      punchline: "I thought ‘That’s not very mature’"
+      },
+    ];
+  }
 }
+
